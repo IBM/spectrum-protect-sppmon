fddd4a65989263973ad0aab925bc770ed7653e68
Niels Korschinsky <Niels.Korschinsky@ibm.com>
2021-03-29 12:07:57 +0200

    setup requirements update

--- scripts/installScript/setupRequirements.sh
+++ scripts/installScript/setupRequirements.sh
@@ -18,7 +18,26 @@ setupRequirements() {
         else
             echo "> Yum installed."
     fi
-
+    echo "> Logging System information"
+    echo -e "-------------------------------System Information----------------------------"
+    echo -e "Hostname:\t\t"`hostname`
+    echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
+    echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
+    echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
+    echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
+    echo -e "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
+    echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi `
+    echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+    echo -e "Kernel:\t\t\t"`uname -r`
+    echo -e "Architecture:\t\t"`arch`
+    echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+    echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+    echo -e "System Main IP:\t\t"`hostname -I`
+    echo -e "----------------------------------Disk--------------------------------------"
+    df -Ph
+    echo -e "-------------------------------Package Updates-------------------------------"
+    yum updateinfo summary | grep 'Security|Bugfix|Enhancement'
+    rowLimiter
 }
 
 # Start if not used as source
