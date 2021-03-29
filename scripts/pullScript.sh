0cae467a7c03226fb94e68f4158372a009051569
Niels Korschinsky <Niels.Korschinsky@ibm.com>
2021-03-29 12:12:09 +0200

    change branch

--- scripts/pullScript.sh
+++ scripts/pullScript.sh
@@ -4,6 +4,6 @@ path=$(dirname "$(readlink -f "$0")")
 cd $path
 
 git fetch
-git reset --hard origin/install_script_setup
+git reset --hard origin/install_script
 chmod +x "./installer.sh"
 chmod +x "./pullScript.sh"
