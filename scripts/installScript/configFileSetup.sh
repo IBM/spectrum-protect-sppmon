2c327ece3a001275fa0e4c2e6736941df9e83e8a
Niels Korschinsky <Niels.Korschinsky@ibm.com>
2021-03-30 14:36:15 +0200

    regex fix

--- scripts/installScript/configFileSetup.sh
+++ scripts/installScript/configFileSetup.sh
@@ -51,7 +51,7 @@ configFileSetup() {
         local spp_retention
         while true; do
             promptLimitedText "How long are the JobLogs saved within the Server? (Format: 48h, 60d, 2w)" spp_password "60d"
-            if [[ $spp_retention =~ ^[0-9]+[hdw]$ ]]; then
+            if [[ "${spp_retention}" =~ ^[0-9]+[hdw]$ ]]; then
                 break
             else
                 echo "The format is incorrect. Please try again."
