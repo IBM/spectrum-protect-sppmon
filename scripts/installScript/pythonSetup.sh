aa627be0861849ca05c181b648477f78d2499556
Niels Korschinsky <Niels.Korschinsky@ibm.com>
2021-04-14 13:03:03 +0200

    python finished?

--- scripts/installScript/pythonSetup.sh
+++ scripts/installScript/pythonSetup.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+currentInstallCheck() {
+    echo "> Verifying the installed python version"
+    local python_old_path=$(which python)
+
+    local current_ver=$(python -V 2>&1)
+    # required version is lower then newly installed -> code does work with 3.8, but latest version is better.
+    local required_ver="3.8.2"
+
+    if [ "$(printf '%s\n' "$required_ver" "$current_ver" | sort -V | head -n1)" = "$required_ver" ]; then
+        echo "> Compatible Python version installed ($current_ver > $required_ver)."
+
+        echo "> Creating systemlink to /usr/bin/python3"
+        checkReturn ln -sf "$python_old_path" /usr/bin/python3
+        return 0
+    else command -v python3 &> /dev/null ; then
+        local python_old_path=$(which python3)
+        local current_ver=$(python3 -V 2>&1)
+
+        if [ "$(printf '%s\n' "$required_ver" "$current_ver" | sort -V | head -n1)" = "$required_ver" ]; then
+            echo "> Compatible Python version installed ($current_ver > $required_ver)."
+            return 0
+        fi
+    fi
+
+    # This uses the latest python 3 install if available -> this version does matter the most.
+    echo "> Current version does not match the requirements ($current_ver < $required_ver)"
+    return 1
+}
+
 pythonSetup() {
 
     rowLimiter
@@ -20,47 +49,52 @@ pythonSetup() {
     checkReturn sudo yum -y install openssl-devel bzip2-devel libffi-devel
     checkReturn sudo yum -y install wget
 
-    echo "> Verifying the installed python version"
-    local python_old_path=$(which python)
+    # check for current python install
+    if ! currentInstallCheck ; then
 
-    local current_ver=$(python -V 2>&1)
-    local required_ver="3.8.2
-    "
-    if [ "$(printf '%s\n' "$required_ver" "$current_ver" | sort -V | head -n1)" = "$required_ver" ]; then
-        echo "> Compatible Python version installed ($current_ver > $required_ver)."
+        echo "> Installing Python3.9"
 
-    else
-        echo "> Installing compatible python version. Current install does not match requirements ($current_ver < $required_ver)"
         checkReturn mkdir -p /tmp/python392
         checkReturn cd /tmp/python392/
         checkReturn wget https://www.python.org/ftp/python/3.9.2/Python-3.9.2.tgz
+        # TODO get without internet
 
         checkReturn cd /tmp/python392/
         checkReturn tar -xf Python-3.9.2.tgz
         checkReturn cd /tmp/python392/Python-3.9.2
         checkReturn ./configure --enable-optimizations --prefix=/usr
 
-        checkReturn sudo make altinstall
+        # Only set alternatives if python 2.7 is installed
+        if command -v python2.7 &> /dev/null ; then
+            echo "> Configuring alternatives between python2.7 (yum) and python3.9 (sppmon)."
+            checkReturn sudo make altinstall
 
-        sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 2
-        sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+            checkReturn sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 2
+            checkReturn sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+            checkReturn sudo update-alternatives --set python /usr/bin/python2.7
+        else
+            checkReturn sudo make install
+        fi
 
-        current_ver=$(python -V 2>&1)
+        echo "> Creating systemlink to /usr/bin/python3"
+        ln -sf /usr/bin/python3.9 /usr/bin/python3
+
+        # Confirming install
+
+        current_ver=$(python3 -V 2>&1)
         if [ "$(printf '%s\n' "$required_ver" "$current_ver" | sort -V | head -n1)" = "$required_ver" ]; then
             echo "> Python install sucessfull."
         else
-            echo "> Python install unsucessfull. Aborting"
+            echo "> Python install unsucessfull."
             abortInstallScript
         fi
-
-        echo "> Configuring alternative switch between old and new python version"
     fi
 
     echo "> Checking pip version"
     checkReturn  python3 -m pip install --upgrade pip
 
     echo "> Installing required packages"
-    checkReturn pip install -r $mainPath/../python/requirements.txt
+    checkReturn pip3 install -r $mainPath/../python/requirements.txt
 
     echo "Finished Python installation Setup"
 
